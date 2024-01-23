617a2044a1c524c4aa174ec195502e4d6a2b8df7
Illia Roman <roman.illia@lll.kpi.ua>
2024-01-23 12:15:13 +0200

    Add linter and formatter

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+   env: {
+      es2021: true,
+      node: true,
+   },
+   extends: ['eslint:all', 'plugin:sonarjs/recommended'],
+   plugins: ['sonarjs'],
+   overrides: [
+      {
+         env: {
+            node: true,
+         },
+         files: ['.eslintrc.{js,cjs}'],
+         parserOptions: {
+            sourceType: 'script',
+         },
+      },
+   ],
+   parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+   },
+   rules: {
+      'one-var': 'off',
+   },
+   ignorePatterns: ['.eslintrc.*'],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+const config = {
+   arrowParens: 'avoid',
+   bracketSameLine: true,
+   trailingComma: 'es5',
+   tabWidth: 3,
+   semi: false,
+   singleQuote: true,
+   printWidth: 80,
+
+   overrides: [
+      {
+         files: '*.json',
+         options: {
+            tabWidth: 2,
+         },
+      },
+   ],
+}
+
+export default config
